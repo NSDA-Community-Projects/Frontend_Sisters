32c866206d5a6b3d633a2d27bbbc52634314d04a
siham <sihamkassim735@gmail.com>
2025-07-19 20:53:17 +0300

    feat:fixed file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import ContactSection from './components/ContactSection';
+import ContactSection from '../src/Section/ContactSection';
 
-import Navbar from "./components/navbar"
-import Home from "./components/Home"
-import About from "./components/about"
-import Features from "./components/Features"
+import About from "../src/Section//about";
+import Features from "../src/Section/Features";
+import Home from "../src/Section/Home";
+import Navbar from "../src/Section/navbar";
 function App() {
   return (
     <div className="font-sans bg-white min-h-screen">
